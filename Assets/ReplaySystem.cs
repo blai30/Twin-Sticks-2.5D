885c69ca43cd1781230f4781fb445ec7126d5ed8
Brian Lai <brianlai30@outlook.com>
2018-03-22 23:01:12 -0700

    Added code to ReplaySystem.cs

--- Assets/ReplaySystem.cs
+++ Assets/ReplaySystem.cs
@@ -6,18 +6,34 @@ public class ReplaySystem : MonoBehaviour {
 
     private const int bufferFrames = 100;
     private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
 
     void Start() {
-        MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update() {
-        
+        Record();
     }
 
+    void PlayBack() {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+    }
+
+    void Record() {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        print("Writing frame " + frame);
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
 }
 
-public class MyKeyFrame {
+/// <summary>
+/// A structure for storing time, position, and rotation
+/// </summary>
+public struct MyKeyFrame {
 
     public float frameTime;
     public Vector3 position;
