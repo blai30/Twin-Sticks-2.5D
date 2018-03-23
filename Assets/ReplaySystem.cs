8b19c6e8f0ae4b014eaa6e7046cdd7445f947616
Brian Lai <brianlai30@outlook.com>
2018-03-22 22:51:16 -0700

    Added ReplaySystem.cs

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+
+    void Start() {
+        MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+    }
+
+    void Update() {
+        
+    }
+
+}
+
+public class MyKeyFrame {
+
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot) {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+
+}
